d521d4fec7ce93fd07f1240ef9da52084441a4a4
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2025-09-16 23:32:26 +0530

    Time: 77 ms (5.09%), Space: 138 MB (5.56%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
@@ -3,7 +3,6 @@ public:
         long long lcm(long long a, long long b) {
     return (a / gcd(a, b)) * b;
 }
-
 vector<int> replaceNonCoprimes(vector<int>& nums) {
     stack<long long> st;
     
@@ -16,19 +15,15 @@ vector<int> replaceNonCoprimes(vector<int>& nums) {
             long long top2 = st.top();
             
             if (gcd(top1, top2) > 1) {
-                // Merge them
                 long long merged = lcm(top1, top2);
                 st.pop();
                 st.push(merged);
             } else {
-                // Not mergeable, push back top1 and break
                 st.push(top1);
                 break;
             }
         }
     }
-    
-    // Build the result from the stack
     vector<int> result(st.size());
     for (int i = st.size() - 1; i >= 0; --i) {
         result[i] = st.top();
