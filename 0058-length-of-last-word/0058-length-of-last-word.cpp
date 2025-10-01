3764c95757b5fe4399ecbbc75853ff4f93d0a6ad
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2025-10-01 11:35:17 +0530

    Time: 0 ms (100%), Space: 9 MB (10.31%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int end=s.find_last_not_of(' ');
+        int start=s.find_last_of(' ',end);
+        return (end-start);
+        
+    }
+};
