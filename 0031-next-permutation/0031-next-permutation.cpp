a1a2eb9590157b5510e7511cb80d8b358d54c31e
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2025-10-02 10:41:08 +0530

    Time: 0 ms (100%), Space: 15.8 MB (33.16%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,13 +3,16 @@ public:
     void nextPermutation(vector<int>& nums) {
     int n = nums.size();
     int i = n - 2;
-    while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+    while (i >= 0 && nums[i] >= nums[i + 1]) 
+    i--;
 
     if (i >= 0) {
         int j = n - 1;
-        while (j > i && nums[j] <= nums[i]) j--;
+        while (j > i && nums[j] <= nums[i]) 
+        j--;
         swap(nums[i], nums[j]);
     }
     reverse(nums.begin() + i + 1, nums.end());
+
 }
 };
