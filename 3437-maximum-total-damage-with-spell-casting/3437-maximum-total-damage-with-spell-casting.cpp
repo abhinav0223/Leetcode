d3efd8911509b20a5bf1808dfa00f35b4943c5b9
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2025-10-11 20:49:27 +0530

    Time: 272 ms (73.48%), Space: 217.6 MB (31.54%) - LeetHub

---
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long maximumTotalDamage(vector<int>& power) {
+        unordered_map<long long, long long> freq;
+        freq.reserve(power.size() * 2);
+
+        for (int v : power) freq[v]++;
+
+        vector<long long> vals;
+        vals.reserve(freq.size());
+        for (auto &p : freq) vals.push_back(p.first);
+        sort(vals.begin(), vals.end());
+
+        vector<long long> sumAtVal(vals.size());
+        for (int i = 0; i < vals.size(); ++i)
+            sumAtVal[i] = vals[i] * freq[vals[i]];
+
+        int n = vals.size();
+        vector<long long> dp(n, 0);
+        dp[0] = sumAtVal[0];
+
+        for (int i = 1; i < n; ++i) {
+            // binary search for last index j where vals[j] <= vals[i] - 3
+            int j = upper_bound(vals.begin(), vals.begin() + i, vals[i] - 3) - vals.begin() - 1;
+            long long take = sumAtVal[i] + (j >= 0 ? dp[j] : 0LL);
+            long long skip = dp[i - 1];
+            dp[i] = max(skip, take);
+        }
+
+        return dp[n - 1];
+    }
+};
