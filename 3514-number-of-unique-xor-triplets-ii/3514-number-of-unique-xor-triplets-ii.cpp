133b3fa15ceb29affc87b0659c5e6abc07d8a0ef
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2026-07-24 10:16:06 +0545

    Time: 517 ms (75.28%), Space: 36.7 MB (73.6%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        const int MAXV = 2048; 
+        vector<bool> present(MAXV, false);
+        for (int v : nums) present[v] = true;
+
+        vector<int> V;
+        for (int v = 0; v < MAXV; v++)
+            if (present[v]) V.push_back(v);
+        vector<bool> S2(MAXV, false);
+        for (int a : V)
+            for (int b : V)
+                S2[a ^ b] = true;
+        vector<bool> S3(MAXV, false);
+        for (int x = 0; x < MAXV; x++) {
+            if (!S2[x]) continue;
+            for (int c : V)
+                S3[x ^ c] = true;
+        }
+
+        int count = 0;
+        for (int x = 0; x < MAXV; x++)
+            if (S3[x]) count++;
+
+        return count;
+    }
+};
