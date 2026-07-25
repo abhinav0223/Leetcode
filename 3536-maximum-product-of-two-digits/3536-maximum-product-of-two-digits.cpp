cf1b53651b52e4788c6bd4af0eeab8a0c956a85c
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2026-07-25 09:48:40 +0545

    Time: 0 ms (100%), Space: 8.6 MB (74.03%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -8,10 +8,10 @@ public:
             if (d >= max1) {
                 max2 = max1;
                 max1 = d;
-            } else if (d > max2) {
-                max2 = d;
+            } else if (d> max2) {
+                max2=d;
             }
         }
-        return max1 * max2;
+        return max1*max2;
     }
 };
