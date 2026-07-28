37f30b90132bcc630023a8379fb5c04e8bcd8d58
Abhinav Raj <125983686+abhinav0223@users.noreply.github.com>
2026-07-28 10:32:38 +0545

    Time: 39 ms (55.6%), Space: 83 MB (5.93%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int cnt[26] = {0};
+        for (char c : s) cnt[c - 'a']++;
+        string half;
+        int mid = -1;
+        for (int i = 0; i < 26; i++) {
+            half += string(cnt[i] / 2, 'a' + i);
+            if (cnt[i] % 2) mid = i;
+        }
+        string res = half;
+        if (mid != -1) res += char('a' + mid);
+        string rev = half;
+        reverse(rev.begin(), rev.end());
+        res += rev;
+        return res;
+    }
+};
